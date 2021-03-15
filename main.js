23fd0909d23c9d9a3bc4b028f4405a9b295814db
OviyaSe <77298020+OviyaSe@users.noreply.github.com>
2021-03-15 13:56:20 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var menu_list_array=["Cheese Pizza","Pepperoni Pizza", "Supreme Pizza", "Hawaiin Chicken Pizza", "Buffalo Chicken Pizza", "Veggie Pizza"];
+
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menu_list'>";
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array;i++){
+        htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>';
+    }
+    htmldata=htmldata+"</ol>";
+    document.getElementById("display_menu").innerHTML=htmldata;
+}
+
+function add_item() {
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata = "<section class='cards'>";
+    for (var i = 0; i < menu_list_array.length; i++) {
+        htmldata = htmldata + "<div class='cards'>"+"<img src='Images/pizzaImg.png'/>" + menu_list_array[i] + "</div>";
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmldata;
+}
